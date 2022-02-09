79a818872f6b3cc5bd53ebac8f03b591c1e8473f
Sudipta <hisudipta26@gmail.com>
2022-02-10 01:28:28 +0530

    feat: updated return change table

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
 const billAmount = document.querySelector('#bill-amount');
+const nextButton = document.querySelector('#next-button');
+
 const cashGiven = document.querySelector('#cash-given');
 const checkButton = document.querySelector('#check-button');
-const errorMessage = document.querySelector('#error-message');
+
+const errorMessage = document.querySelector('.error-message');
+
+const cashGivenDiv = document.querySelector('.cashGivenInput');
+const changeReturnDiv = document.querySelector('.change-return');
+
 const noOfNotes = document.querySelectorAll('.no-of-notes');
 
 const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 
-checkButton.addEventListener('click', function  validateBillAndCashAmount(){
+nextButton.addEventListener('click', function  validateBillAmount(){
     hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+    if(Number(billAmount.value) > 0){
+        nextButton.style.display = "none";
+        cashGivenDiv.style.display = "block";
+    } else {
+        showMessage("Invalid Bill Amount! Enter a valid Bill Amount");
+    }
+});
+
+checkButton.addEventListener('click', function  validateCashGiven(){
+    clearNoOfNotes();
+    hideMessage();
+    if(Number(billAmount.value)>0 && Number(cashGiven.value)>0){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
+            const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
+            if(amountToBeReturned<1){
+                showMessage("No amount should be returned");
+                changeReturnDiv.style.display = "none";
+            } else{
+                calculateChange(amountToBeReturned);
+            }
         } else{
             showMessage("Cash amount should be more than or equal to the bill amount");
         }
-    } else {
-        showMessage("Invalid Bill Amount!");
-    }
+    } else{
+        showMessage("Invalid input! Enter valid Bill Amount & Cash Given to continue");
+    }  
 });
+
+function calculateChange(amountToBeReturned){
+    changeReturnDiv.style.display = "block";
+    for(let i = 0; i < availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+//if check button clicked without refreshing the page, clear the no of notes values on the screen
+function clearNoOfNotes(){
+    for(let notes of noOfNotes){
+        notes.innerText = "";
+    }
+}
 
 function hideMessage(){
     errorMessage.style.display = "none";
@@ -27,12 +66,5 @@ function hideMessage(){
 function showMessage(message){
     errorMessage.style.display = "block";
     errorMessage.innerText = message;
-}
-
-function calculateChange(amountToBeReturned){
-    for(let i = 0; i < availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
-    }
+    changeReturnDiv.style.display = "none";
 }
